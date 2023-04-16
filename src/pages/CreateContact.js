16b4b728ecaa03013e02390e92ae9d1af66a8789
Aman Singh <88033748+aman-s-20@users.noreply.github.com>
2023-04-16 18:25:51 +0530

    updated server url

--- src/pages/CreateContact.js
+++ src/pages/CreateContact.js
@@ -24,7 +24,7 @@ const CreateContact = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const res = await fetch(`http://localhost:8000/api/contact`, {
+    const res = await fetch(`https://cms-backend-0bmb.onrender.com/contact`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
